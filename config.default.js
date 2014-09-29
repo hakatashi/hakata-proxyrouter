aa92617af96f97df463a9ead48e8cfc3e58fc0b3
hakatashi <hakatasiloving@gmail.com>
2014-09-29 16:28:10 +0900

    Add support for hostname routings

--- config.default.js
+++ config.default.js
@@ -1,14 +1,39 @@
 module.exports = {
-	host: 'localhost',
-	port: 10721,
+	listen: 10721,
 	route: {
-		test: {
-			host: 'localhost',
-			port: 10722
+		'example.com': {
+			'/': {
+				type: 'static',
+				target: 'static/example.com'
+			},
+			'/test': {
+				type: 'proxy',
+				target: 'http://localhost:10722'
+			},
+			'/api': {
+				type: 'proxy',
+				target: 'http://localhost:10723'
+			},
 		},
-		api: {
-			host: 'localhost',
-			port: 10723
+		'example.net': {
+			'/': {
+				type: 'static',
+				target: 'static/example.net'
+			},
+			'/test': {
+				type: 'proxy',
+				target: 'http://localhost:10724'
+			},
+			'/api': {
+				type: 'proxy',
+				target: 'http://localhost:10725'
+			},
+		},
+		'_any': {
+			'/': {
+				type: 'static',
+				target: 'static/default'
+			},
 		},
 	},
 };
--- hakata-proxyrouter.js
+++ hakata-proxyrouter.js
@@ -9,29 +9,57 @@ var app = express();
 var proxy = httpProxy.createProxyServer();
 
 var proxyRouter = function (req, res, next) {
-	for (var path in config.route) {
-		if (config.route.hasOwnProperty(path)) {
-			if (req.url.startsWith('/' + path + '/')) {
-				req.url = req.url.slice(path.length + 1);
-				proxy.web(req, res, {
-					target: config.route[path]
-				});
-				return;
-			}
+	// determine hostname
+
+	var routes;
+
+	if (config.route.hasOwnProperty(req.hostname)) {
+		routes = config.route[req.hostname];
+	} else if (config.route.hasOwnProperty('_any')) {
+		routes = config.route._any;
+	} else {
+		return next();
+	}
+
+	// determine route
+
+	var route;
+
+	for (var path in routes) if (routes.hasOwnProperty(path)) {
+		if (path !== '/' && req.url.startsWith(path + '/')) {
+			req.url = req.url.slice(path.length);
+			route = routes[path];
+			break;
 		}
 	}
 
-	next();
+	if (!route && routes['/']) {
+		route = routes['/'];
+	}
+
+	if (!route) return next();
+
+	// proxy
+
+	if (route.type === 'proxy') {
+		proxy.web(req, res, {
+			target: route.target,
+		});
+	} else if (route.type === 'static') {
+		express.static(route.target, route.options)(req, res, next)
+	} else {
+		return next();
+	}
 };
 
 app.use(morgan('combined'));
 app.use(proxyRouter);
-app.use(express.static('static'));
-
-app.get('/', function (req, res) {
-	res.send('Router works!');
+// not found
+app.use(function (req, res) {
+	res.status(404);
+	res.send('Requested URL ' + req.originalUrl + ' is not found among hakata-proxyrouter sorry ;(');
 });
 
-var server = app.listen(config.port, function() {
+var server = app.listen(config.listen, function() {
 	console.log('Listening on port %d', server.address().port);
 });
